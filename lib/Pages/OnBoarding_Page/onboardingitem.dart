1a306ffb31a3a7edaf28ae3cbdd14de2b0502387
Aan Kurniawan <aank34424@gmail.com>
2024-04-26 09:03:05 +0700

    feat: [AK] splash & onboarding page done

---
+++ lib/Pages/OnBoarding_Page/onboardingitem.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+import 'OnboardingData.dart';
+class OnboardingItems{
+  List<OnboardingData> items = [
+    OnboardingData(
+        title: "Custom Sendiri",
+        image: "img/on1.png",
+        description: "Tingkatkan pengalaman berbelanja Anda dengan memilih dan menyesuaikan produk sesuai dengan keinginan Anda"),
+
+    OnboardingData(
+        title: "Pembayaran Mudah",
+        image: "img/on2.png",
+        description: "Sederhanakan proses pembayaran Anda dengan memilih payment gateway yang sesuai, dan bayarlah dengan mudah tanpa repot."),
+
+    OnboardingData(
+        title: "Diskon Menarik",
+        image: "img/on3.png",
+        description: "Dapatkan pengalaman belanja yang lebih baik dengan memilih dan menyesuaikan produk sesuai keinginan Anda, serta menikmati diskon dan penawaran khusus lainnya"),
+  ];
+
+}
---
+++ lib/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorValue {
+
+  static const yellow = Color( 0xffF6B60D);
+  static const White = Color( 0xffFAFAFA);
+  static const Black = Color( 0xff000000);
+  static const grey = Color( 0xff7D7D7D);
+
+
+}
